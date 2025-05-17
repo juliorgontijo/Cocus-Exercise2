863b34e9e15a4da3d098862d38e7b8662cbdaf43
Julio Gontijo <juliorgontijo@gmail.com>
2025-05-18 00:32:25 +0100

    Add project files.

---
+++ CocusExercise2/Program.cs
@@ -0,0 +1,67 @@
+﻿namespace CocusExercise2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var message1 = new MessageA();
+            message1.Handle();
+
+            var message2 = new MessageB();
+            message2.Handle();
+
+            var message3 = new MessageC();
+            message3.Handle();
+        }
+    }
+
+    public abstract class Message
+    {
+        public abstract void Handle();
+    }
+
+    public class MessageA : Message
+    {
+        public override void Handle()
+        {
+            MyCustomMethodOnA();
+        }
+
+        public void MyCustomMethodOnA() 
+        {
+            Console.WriteLine("MyCustomMethodOnA");
+        }
+    }
+
+    public class MessageB : Message
+    {
+        public override void Handle()
+        {
+            MyCustomMethodOnB();
+            SomeAdditionalMethodOnB();
+        }
+
+        public void MyCustomMethodOnB()
+        {
+            Console.WriteLine("MyCustomMethodOnB");
+        }
+
+        public void SomeAdditionalMethodOnB() 
+        {
+            Console.WriteLine("SomeAdditionalMethodOnB");
+        }
+    }
+
+    public class MessageC : Message
+    {
+        public override void Handle()
+        {
+            MyCustomMethodOnC();
+        }
+
+        public void MyCustomMethodOnC() 
+        {
+            Console.WriteLine("MyCustomMethodOnC");
+        }
+    }
+}
